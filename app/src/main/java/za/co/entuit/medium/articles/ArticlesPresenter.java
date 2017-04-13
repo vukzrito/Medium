52cce8940fd30e4673585ebb629edd9acedc950b
Rito <rvukela@jhb.dvt.co.za>
2017-04-13 08:22:08 +0200

    Add articles presenter

--- app/src/main/java/za/co/entuit/medium/articles/ArticlesPresenter.java
+++ app/src/main/java/za/co/entuit/medium/articles/ArticlesPresenter.java
@@ -1,17 +1,41 @@
 package za.co.entuit.medium.articles;
 
+import java.util.List;
+
+import za.co.entuit.medium.data.Article;
+import za.co.entuit.medium.data.ArticlesRepository;
+
 /**
  * Created by RVukela on 2017/04/13.
  */
 
 public class ArticlesPresenter implements ArticlesContract.UserActionsListener {
+    private ArticlesContract.View view;
+    private ArticlesRepository articlesRepository;
+
+    public ArticlesPresenter(ArticlesContract.View view, ArticlesRepository articlesRepository) {
+        this.view = view;
+        this.articlesRepository = articlesRepository;
+    }
+
     @Override
     public void loadArticles(boolean forceUpdate) {
+        view.showProgressIndicator(true);
+        articlesRepository.loadArticles(new ArticlesRepository.LoadArticlesCallback() {
+            @Override
+            public void onLoaded(List<Article> articles) {
+                view.showArticles(articles);
+            }
 
+            @Override
+            public void onError(String errorMessage) {
+                view.showErrorMessage(errorMessage);
+            }
+        });
     }
 
     @Override
     public void openArticleDetail(String articleId) {
-
+        view.showArticleDetail(articleId);
     }
 }
---
+++ app/src/main/java/za/co/entuit/medium/data/ArticlesRepositoryImpl.java
@@ -0,0 +1,19 @@
+package za.co.entuit.medium.data;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Created by RVukela on 2017/04/13.
+ */
+
+public class ArticlesRepositoryImpl implements ArticlesRepository {
+    @Override
+    public void loadArticles(@NonNull LoadArticlesCallback callback) {
+
+    }
+
+    @Override
+    public void loadArticle(@NonNull LoadArticleCallback callback) {
+
+    }
+}
