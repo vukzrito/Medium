ed34ec2b5e297869b45bb70a1bb6f783b40f93e3
Rito <rvukela@jhb.dvt.co.za>
2017-04-14 18:26:04 +0200

    Add now playing notification

--- app/src/main/java/za/co/entuit/medium/stream/StreamFragment.java
+++ app/src/main/java/za/co/entuit/medium/stream/StreamFragment.java
@@ -1,5 +1,9 @@
 package za.co.entuit.medium.stream;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -12,7 +16,10 @@ import android.view.ViewGroup;
 
 import java.io.IOException;
 
+import za.co.entuit.medium.MainActivity;
 import za.co.entuit.medium.R;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 /**
  * Created by RVukela on 2017/04/14.
@@ -48,5 +55,27 @@ public class StreamFragment extends Fragment {
         }
 
         mediaPlayer.start();
+        initNotificationBar();
+
+
+    }
+
+    void initNotificationBar(){
+        Intent notIntent = new Intent(getContext(), MainActivity.class);
+        notIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getContext(),0,
+                notIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification.Builder builder = new Notification.Builder(getContext());
+        builder.setContentIntent(pendingIntent)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setOngoing(true)
+                .setContentTitle("Medium")
+                .setContentText("Now playing");
+        Notification notification = builder.build();
+        NotificationManager notificationManager = (NotificationManager)getActivity()
+                .getSystemService(NOTIFICATION_SERVICE);
+
+
+        notificationManager.notify(0, notification);
     }
 }
